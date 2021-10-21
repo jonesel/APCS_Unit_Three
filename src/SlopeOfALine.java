fc10996193140726876af08f8527a8fcb7805e6d
EliFJ <jonesel@ssfs.org>
2021-10-21 11:02:46 -0600

    Calculated slope

--- src/SlopeOfALine.java
+++ src/SlopeOfALine.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SlopeOfALine {
 
     /**
@@ -8,14 +10,30 @@ public class SlopeOfALine {
      * @param y2 - y-coordinate of second point
      * @return - Different strings based on whether the line is vertical or not
      */
-    public static String slope(int x1, int y1, int x2, int y2) {
-
-        // update the line below to return a properly formatted string
-        return "";
+    public static String slope(int x1, int y1, int x2, int y2)
+    {
+        if (x2-x1!=0)
+        {
+            double slope = ((double) y2-y1)/(x2-x1);
+            return "The line defined by the points "+"("+x1+","+y1+") and "+"("+x2+","+y2+") has a slope of "+slope;
+        }
+        else
+        {
+            return "The line defined by the points "+"("+x1+","+y1+") and"+"("+x2+","+y2+") is a vertical line and the slope is undefined.";
+        }
     }
 
     public static void main(String[] args) {
-        // Get input from the user (you will need to create a Scanner object)
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the x1 coordinate: ");
+        int x1 = scan.nextInt();
+        System.out.println("Please enter the corresponding y1 coordinate: ");
+        int y1 = scan.nextInt();
+        System.out.println("Please enter the x2 coordinate: ");
+        int x2 = scan.nextInt();
+        System.out.println("Please enter the corresponding y2 coordinate: ");
+        int y2 = scan.nextInt();
+        System.out.println(slope(x1,y1,x2,y2));
         // Call and print the result of the slope method as shown in the instructions.
     }
 }
