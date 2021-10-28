0e0985f3bc39787840860b3dc5c5635ece77f532
EliFJ <jonesel@ssfs.org>
2021-10-28 09:01:01 -0600

    found if inputs are between two numbers

--- src/LoneTeen.java
+++ src/LoneTeen.java
@@ -1,5 +1,15 @@
 public class LoneTeen {
+    public static boolean loneTeen(int num1, int num2) {
+        if (num1 <= 19 && num1 >= 13 && num2 < 13 || num2 > 19) {
+            return true;
+        }
+        if (num1 < 13 || num1 > 19 && num2 >= 13 && num2 <= 19) {
+            return true;
+        }
+        return false;
 
-    // In the space below, write the loneTeen method. Make sure the signature matches
-    // the one given in the instructions.
+
+        // In the space below, write the loneTeen method. Make sure the signature matches
+        // the one given in the instructions.
+    }
 }
